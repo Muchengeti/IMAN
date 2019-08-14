1ccb28579332799382cb2a638537d7e424a7f12b
Muchengeti <matindemv@outlook.com>
2019-08-14 12:10:07 +1200

    Create and migrate models and admin superuser

--- backend/src/Posts/admin.py
+++ backend/src/Posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Event, Article
 
 # Register your models here.
+django_models = [Event, Article]
+admin.site.register(django_models)
--- backend/src/Posts/models.py
+++ backend/src/Posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=120)
+    author = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField() 
+
+    def __str__(self):
+        return self.title
+
+class Event(models.Model):
+    title = models.CharField(max_length=120)
+    author = models.CharField(max_length=120)
+    group = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    event_location = models.CharField(max_length=120)
+    event_time = models.CharField(max_length=120)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
